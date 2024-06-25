75dfa939a852dcbd2f8ec0b3b4cbed996d723d84
Rauniyar-shovit <rauniyar.shovit62@gmail.com>
2024-06-25 14:03:48 +1000

    added the new budget form

--- app/(routes)/dashboard/budgets/_components/CreateBudget.tsx
+++ app/(routes)/dashboard/budgets/_components/CreateBudget.tsx
@@ -1,4 +1,6 @@
-import React from "react";
+"use client";
+
+import React, { useState } from "react";
 import {
   Dialog,
   DialogContent,
@@ -7,8 +9,15 @@ import {
   DialogTitle,
   DialogTrigger,
 } from "@/components/ui/dialog";
-
+import EmojiPicker from "emoji-picker-react";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
 const CreateBudget = () => {
+  const [emojiIcon, setEmojiIcon] = useState("😊");
+  const [openEmojiPicker, setOpenEmojiPicker] = useState(false);
+  const [name, setName] = useState<string | undefined>();
+  const [amount, setAmount] = useState<number | undefined>();
+
   return (
     <div>
       <Dialog>
@@ -20,10 +29,48 @@ const CreateBudget = () => {
         </DialogTrigger>
         <DialogContent>
           <DialogHeader>
-            <DialogTitle>Are you absolutely sure?</DialogTitle>
+            <DialogTitle>Create New Budget</DialogTitle>
             <DialogDescription>
-              This action cannot be undone. This will permanently delete your
-              account and remove your data from our servers.
+              <div className="mt-5">
+                <Button
+                  variant="outline"
+                  size={"lg"}
+                  className="text-lg"
+                  onClick={() => setOpenEmojiPicker(() => !openEmojiPicker)}
+                >
+                  {emojiIcon}
+                </Button>
+                <div className="absolute">
+                  <EmojiPicker
+                    open={openEmojiPicker}
+                    onEmojiClick={(e) => {
+                      setEmojiIcon(e.emoji);
+                      setOpenEmojiPicker(false);
+                    }}
+                  />
+                </div>
+                <div className="mt-2">
+                  <h2 className="text-black font-medium my-1">Budget Name</h2>
+                  <Input
+                    placeholder="e.g. Home Decor"
+                    onChange={(e) => setName(e.target.value)}
+                  />
+                </div>
+                <div className="mt-2">
+                  <h2 className="text-black font-medium my-1">Budget Amount</h2>
+                  <Input
+                    placeholder="e.g. $5000"
+                    type="number"
+                    onChange={(e) => setAmount(+e.target.value)}
+                  />
+                </div>
+                <Button
+                  className="mt-5 w-full bg-indigo-600"
+                  disabled={!name || !amount}
+                >
+                  Create Budget
+                </Button>
+              </div>
             </DialogDescription>
           </DialogHeader>
         </DialogContent>
